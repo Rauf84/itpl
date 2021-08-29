58b2be9a830acd1072ea39a3d7e87d9842590b71
Rauf84 <rauf@karimli.se>
2021-08-29 15:04:35 +0200

    send function - ok

--- contact.js
+++ contact.js
@@ -1,12 +1,14 @@
-function sendMail (params) {
-    var tempParams = {
-        from_name: document.getElementById("from_name").value,
-        to_name: document.getElementById("to_name").value,
-        message: document.getElementById("message").value,
-    };
 
-    emailjs.send('service_a98659k','template_pkgugf9', tempParams)
-    .then (function(res){
-        console.log("success", res.status);
-    });
+function send(){
+    emailjs.send("service_a98659k","template_pkgugf9",{
+        to_name: "Rauf",
+        from_name: document.querySelector("#from_name").value,
+        message: document.querySelector("#message").value,
+    }).then (response=>{
+        console.log("success", response.status);
+    }, (error)=>{
+        console.log(error);
+    })
 }
+
+document.querySelector(".send").addEventListener("click",send)
