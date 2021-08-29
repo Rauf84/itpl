1d3f5874f30b8a6f27bcd4fa1c6c29a636e4fe27
Rauf84 <rauf@karimli.se>
2021-08-29 22:04:30 +0200

    last version 210829

--- contact.js
+++ contact.js
@@ -1,11 +1,22 @@
+function success(){
+    swal({
+        title: "Thank you!",
+        text: "We will get back to you within 24 hours",
+        icon: "success",
+        button:"Close",
+    });
+}
+
 
 function send(){
     emailjs.send("service_a98659k","template_pkgugf9",{
         to_name: "Rauf",
         from_name: document.querySelector("#from_name").value,
+        from_email: document.querySelector("#from_email").value,
         message: document.querySelector("#message").value,
     }).then (response=>{
         console.log("success", response.status);
+        success();
     }, (error)=>{
         console.log(error);
     })
